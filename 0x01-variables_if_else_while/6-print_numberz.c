d8f834aaeb1cd754edc62782cf41c6fff9c81b7a
AbdiAbader <abdiprocode@gmail.com>
2022-06-03 04:09:00 -0700

    all commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * base 10 numbers
+ *
+ * Abdi
+ */
+int main(void)
+{
+  int base;
+  for(base=0; base<10; base++)
+    {
+      putchar(base + '0');
+    }
+  putchar('\n');
+  return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * prints in lower case
+ *
+ *
+ */
+
+int main()
+{
+  int alpha;
+  
+ for(alpha=122; alpha>=97; alpha--)
+    {
+      putchar(alpha);
+	}
+ putchar('\n');
+  return (0);
+}
+
+
