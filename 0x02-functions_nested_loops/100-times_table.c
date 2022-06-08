f3266e835e36046cccdf2d9af3e68a75cb30031c
AbdiAbader <abdiprocode@gmail.com>
2022-06-08 03:36:22 -0700

    time3s

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
  */
 void print_times_table(int n)
 {
-int ab, di, multi, ab1, di1, ab2, di2;
+int ab, di, multi;
 if (n >= 0 && n < 15)
 {
 for (ab = 0; ab <= n; ab++)
@@ -22,20 +22,14 @@ _putchar(32);
 if (multi > 9 && multi <= 99)
 {
 _putchar(32);
-ab1 = multi / 10;
-di1 = multi % 10;
-_putchar(ab1 + '0');
-_putchar(di1 + '0');
+_putchar((multi / 10) + '0');
+_putchar((multi % 10) + '0');
 }
 if (multi > 99)
 {
-di2 = multi / 100;
-ab2 = multi / 10;
-ab1 = ab2 % 10;
-di1 = multi % 10;
-_putchar(di2 + 48);
-_putchar(ab1 + 48);
-_putchar(di1 + 48);
+_putchar((multi / 100) + 48);
+_putchar(((multi / 10) / 10) + 48);
+_putchar((multi % 10) + 48);
 }
 else if ( multi <= 9)
 {
