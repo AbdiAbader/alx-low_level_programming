548e3001524cc75eaea5ee2b7af1d476093da8f4
AbdiAbader <abdiprocode@gmail.com>
2022-06-09 07:13:22 -0700

    fizz and buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  *main - print number to 100 replace number devisible by 3 - fizz and 5 -Buzz.
- *
+ *Fizz:for number devisible by 3
+ *Buzz:for number devisible by 5
  *Return: 0 if success
  */
 int main(void)
@@ -9,6 +10,11 @@ int main(void)
 int a;
 for (a = 1; a <= 100; a++)
 {
+if ((a % 3 == 0) && (a % 5 == 0))
+{
+printf("FizzBuzz ");
+continue;
+}
 if ((a % 3) == 0)
 {
 printf("Fizz ");
