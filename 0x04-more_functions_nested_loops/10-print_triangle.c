1ebcc182a1494b518e2368ba7ded41c4301daacc
AbdiAbader <abdiprocode@gmail.com>
2022-06-09 07:30:49 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_triangle - in triangle shape
+ *@size : numbers of #
+ *
+ *Return: Always 0.
+ */
+void print_triangle(int size)
+{
+int a, i, b;
+if (size > 0)
+{
+for (a = 0; a < size; a++)
+{
+for (i = a; i < size; i++)
+{
+_putchar(32);
+}
+for (b = i; b <= i; b++)
+{
+_putchar(35);
+}
+}
+}
+_putchar('\n');
+}
