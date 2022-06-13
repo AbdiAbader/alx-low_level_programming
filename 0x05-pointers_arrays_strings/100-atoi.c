fda5854a372ac9ca68c8bf35c9f9d8f1cd4efbf9
AbdiAbader <abdiprocode@gmail.com>
2022-06-13 03:51:08 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,23 +8,23 @@ int _atoi(char *s)
 {
 int a,  sign = 1;
 unsigned int num = 0;
+for (a = 0; *s != '\0'; a++)
+{
 if (*s == '-')
 {
 sign *= -1;
 s++;
-}
-
-for (a = 0; *s != '\0'; a++)
-{
-if (*s == '-' || *s == '+')
 continue;
+}
 if (*s >= 48 && *s <= 57)
 {
 num = num * 10 + *s - 48;
 s++;
+if (*s < 48 || *s > 57)
+return (num *sign);
 }
 else
-return (num *sign);
+s++;
 }
 return (0);
 }
