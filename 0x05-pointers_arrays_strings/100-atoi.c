bc6100d924c9e1ca0682e395bdfe394f3aa110cd
AbdiAbader <abdiprocode@gmail.com>
2022-06-13 03:40:12 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,22 +6,27 @@
  */
 int _atoi(char *s)
 {
-int a, num = 0, sign = 1;
+int a,  sign = 1;
+unsigned int num = 0;
 if (*s == '-')
 {
 sign *= -1;
+s++;
 }
-if (*s >= 48 && *s <= 57)
-{
+
 for (a = 0; *s != '\0'; a++)
 {
+if (*s == '-' || *s == '+')
+continue;
 if (*s >= 48 && *s <= 57)
 {
 num = num * 10 + *s - 48;
 s++;
-}
+if (*s >= 48 && *s <= 57)
+continue;
 else
 break;
+}
 }
 return (num *sign);
 }
