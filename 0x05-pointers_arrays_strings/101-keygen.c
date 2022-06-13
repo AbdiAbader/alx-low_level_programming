8d35bbd17ccdbf9aa003ab28c4810b31dd08d95d
AbdiAbader <abdiprocode@gmail.com>
2022-06-13 06:09:00 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,12 +3,31 @@
 #include <time.h>
 int main(void)
 {
-int a = 33, b = 122, i;
+  int a = 33, b = 122, i, r = 0;
 srand(time(0));
+ r= rand() % 4;
+ int a[]={33,35,36,38,42,64,94};
 for (i = 0; i < 14 ; i++)
 {
- putchar(rand() % ((a-b)+1)-a);
-}
+if(r == 1)
+{
+  putchar(rand() % (90-65+1)+65);
+  r = rand() % 4;
+ }
+ if( r == 2)
+ {
+ putchar(rand() % (122-97+1)+97);
+ r= rand() % 4;
+ }
+if (r == 3)
+ {
+ putchar(rand() % (57-48+1)+48);
+ }
+if( r == 4)
+{
+  putchar(a[rand() % 8 ]);
+ }
+ }
 printf("\n");
 return (0);
 }
