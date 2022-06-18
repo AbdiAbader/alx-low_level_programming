ac19eb109f5effd0557cea066e11d179cea59747
AbdiAbader <abdiprocode@gmail.com>
2022-06-18 04:25:37 -0700

    key

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,15 +9,19 @@
 int main(void)
 {
   int ab[9]={32, 33, 35, 36, 38, 42, 63, 64, 94};
-  int  i, c = 0, r = 0, d = 0;
+  int  i, r = 0, d = 0;
 srand(time(0));
  r= rand() % 4;
 for (i = 1;d < 2772 ; i++)
 {
-  c++;
-  d = d + (rand() % 128);
-  if ( c < 10)
+  if( i == 1)
     {
+      printf("Tada! Congrats");
+      continue;
+    }
+ 
+  d = d + (rand() % 128);
+ 
 if(r == 1)
 {
   putchar((rand() % (90-65+1))+65);
@@ -39,12 +43,8 @@ else
 
   r= rand() % 4;
  }
-    }
-  else
-    {
-      printf("\n");
-      printf("Tada! Congrats");
-    }}
+      
+      }
 printf("\n");
  
 return (0);
