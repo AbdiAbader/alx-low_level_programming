4298d526f2878108737bf39f909c6051dc938154
AbdiAbader <abdiprocode@gmail.com>
2022-06-20 02:04:58 -0700

    memcpy and other two

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strchr - return the first match address
+ *@s: string
+ *@c: character
+ *Return: Address of the pointer
+ */
+char *_strchr(char *s, char c)
+{
+char *p;
+while (s)
+{
+if (*s == c)
+{
+p = s;
+return (p);
+}
+s++;
+}
+return (0);
+}
