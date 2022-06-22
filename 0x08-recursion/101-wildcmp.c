9db8f4e0da03d0b497652596f9ef3ec8af97db31
AbdiAbader <abdiprocode@gmail.com>
2022-06-22 12:22:14 -0700

    101-wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,23 +1,20 @@
 #include "main.h"
 /**
  *check - compare indetical of two string
- *@s1: input intger 
+ *@s1: input intger
  *@s2: input integer
  *Return: 1 if identical otherwise 0
  */
-int check(char *s1 , char *s2)
+int check(char *s1, char *s2)
 {
+if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+return (0);
 if ((*s2 == '\0' && *s1 == '\0'))
 return (1);
 if (*s2 == *s1)
-return (check(++s1,++s2));
+return (check(s1 + 1, s2 + 1));
 if (*s2 == '*')
-{
-if (*(s2 + 1) == '*')
-return (check(s1,++s2));
-if ((check(++s1,s2)) || (check(s1,++s2)))
-return (1);
-}
+return  ((check(s1 + 1, s2)) || (check(s1, s2 + 1)));
 return (0);
 }
 /**
@@ -29,6 +26,6 @@ return (0);
 int wildcmp(char *s1, char *s2)
 {
 int r;
-r = check(s1,s2);
+r = check(s1, s2);
 return (r);
 }
