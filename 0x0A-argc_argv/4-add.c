89ea1e215f191e2c98585ffc303e4d44b5e9eea0
AbdiAbader <abdiprocode@gmail.com>
2022-06-24 03:55:08 -0700

    aLL

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ *main - prints addition of numbers
+ *@argc: number of arguments.
+ *@argv: char pointer
+ *Return: 0 for success
+ */
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
+{
+int i = 1, add = 0;
+while (i < argc)
+{
+if (atoi(argv[i]))
+add += atoi(argv[i]);
+if (*argv[i] == '0')
+add += atoi(argv[i]);
+else
+{
+printf("Error\n");
+return (1);
+}
+i++;
+}
+printf("%i\n", add);
+return (0);
+}
