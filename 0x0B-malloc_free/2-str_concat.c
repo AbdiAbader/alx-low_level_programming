06646a776842587cd439e8ba397cb1e8e964b5be
AbdiAbader <abdiprocode@gmail.com>
2022-06-26 23:59:10 -0700

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,25 +10,25 @@ char *str_concat(char *s1, char *s2)
 {
 int i = 0, j = 0, a;
 char *p;
+if (s1 != NULL)
+{
 while (s1[j] != '\0')
 j++;
+}
+if (s2 != NULL)
+{
 while (s2[i] != '\0')
 i++;
+}
 p = malloc(sizeof(char) * (j + i + 1));
 if (p == NULL)
 return (0);
 else
 {
-if (*s1 != NULL)
-{
 for (a = 0; a < j; a++)
 p[a] = s1[a];
-}
-if (*s2 != NULL)
-{
 for (a = 0; a < i; a++)
 p[a + j] = s2[a];
-}
 return (p);
 }
 }
