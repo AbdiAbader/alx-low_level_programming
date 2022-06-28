2839b7b4684ee7342d5f4b30738222b033a5eed0
AbdiAbader <abdiprocode@gmail.com>
2022-06-27 23:54:31 -0700

    strtow

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include "main.h"
+void cal(char *str, char **new, int s,int i)
+{
+  int x, len = 0, a, b;
+x = s;
+while (str[x] != ' ')
+{
+x++;
+len++;
+}
+new[i] =(char *)malloc(sizeof(char) * len + 1);
+for (a = i; a <= i; a++)
+{
+for (b = 0; b < len; b++)
+  {
+  new[a][b] = str[s];
+  s++; 
+}
+new[a][b] = '\0';
+}
+  
+}
+/**
+ *strtow - words to array
+ *@str: stings
+ *Return: NULL if empty and fials otherwise pointer address
+ */
+char **strtow(char *str)
+{
+char **new;
+int i = 0,  c = 0, d;
+for (d = 0; str[d] != '\0'; d++)
+  if (str[d] == ' ' && (str[d+1] != ' ' && str[d+1] != '\0'))
+c++;
+new = malloc(sizeof(char*) * c + 1);
+if (new == NULL)
+{
+free(new);
+return (NULL);
+}
+else
+{
+for (d = 0; str[d] != '\0'; d++)
+{
+  if (str[d] == ' ' && (str[d+1] != ' ' && str[d+1] != '\0'))
+  {  cal(str,new,d + 1,i);
+    i++;
+ continue;
+  }
+}
+return (new);
+}
+}
