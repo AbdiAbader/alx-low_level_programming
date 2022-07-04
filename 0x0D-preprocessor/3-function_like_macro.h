abec686cb0cdd5e14ea31e1f400eaebccce05f42
AbdiAbader <abdiprocode@gmail.com>
2022-07-04 04:32:18 -0700

    change to upper case

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _function_like_macro_
-#define _function_like_macro_
-#define ABS(x) ((x) > 0 ? (x) : (-1 * x))
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x) > 0 ? (x) : (-x))
 #endif
