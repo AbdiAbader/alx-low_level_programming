84945d3d8bfb848fde811020dab606116e4edfc7
AbdiAbader <abdiprocode@gmail.com>
2022-07-04 04:51:21 -0700

    new_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include "dog.h"
+#include <stdlib.h>
+int length(char *temp);
+int length(char *temp)
+{
+int len = 0, i = 0;
+while (temp[i] != '\0')
+{
+i++;
+len++;
+}
+return (len);
+}
+/**
+ *new_dog - Store copy of name and owner.
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of the dog
+ *Return: Null if its fail
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t  *copy;
+int  i = 0, len;
+copy = malloc(sizeof(dog_t));
+if (copy == NULL)
+{
+free(copy);
+return (NULL);
+}
+len = length(name);
+copy->name = malloc(sizeof(char) * len + 1);
+while (name[i] != '\0')
+{
+copy->name[i] = name[i];
+i++;
+}
+copy->name[i] = name[i];
+len = length(owner);
+copy->owner = malloc(sizeof(char) * len + 1);
+while (owner[i] != '\0')
+{
+copy->owner[i] = owner[i];
+i++;
+}
+copy->owner[i] = owner[i];
+copy->age = age;
+return (copy);
+}
