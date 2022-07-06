0a43241349f3bd82d14e69bf5b685815722d21c4
AbdiAbader <abdiprocode@gmail.com>
2022-07-06 06:25:04 -0700

    all

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "3-calc.h"
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
+{
+char *o;
+int num1, num2;
+if (argc != 4)
+{
+printf("%i Error\n", argc);
+exit(98);
+}
+o = argv[2];
+if (get_op_func(o) == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+if ((*o == '/' || *o == '%') && (atoi(argv[3]) == 0))
+{
+printf("Error\n");
+exit(100);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+printf("%i\n",get_op_func(o)(num1, num2));
+return (0);
+}
