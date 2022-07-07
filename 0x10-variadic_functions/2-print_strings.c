dfb35ceb221302c117cd680a8628c495ffba53c1
AbdiAbader <abdiprocode@gmail.com>
2022-07-07 03:54:08 -0700

    correction of NULL)

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,15 +7,17 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+char *s;
 unsigned int i = 0;
 va_list args;
 va_start(args, n);
 while (i < n)
 {
-if (va_arg(args, char *) == NULL)
+s = va_arg(args, char *);
+if (s == NULL)
 printf("(nil)");
 else
-printf("%s", va_arg(args, char *));
+printf("%s", s);
 if (i != n - 1 && separator != NULL)
 printf("%s", separator);
 i++;
