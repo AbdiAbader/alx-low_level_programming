8a918c83b7f5adb841968e31a8f304b23cbf1be0
AbdiAbader <abdiprocode@gmail.com>
2022-07-07 12:10:34 -0700

    remove one if cond

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,40 +1,44 @@
 #include "variadic_functions.h"
+/**
+ *print_all - print s all argumetns if matchs withs format
+ *@format: chars
+ *Return: void
+ */
 void print_all(const char * const format, ...)
 {
-  char *pe;
+char *pe, *se = "";
 int i = 0;
 va_list args;
-va_start(args,format);
-if(format)
+va_start(args, format);
+if (format)
 {
-while(format[i])
+while (format[i])
 {
-switch(format[i])
+switch (format[i])
 {
 case 'c':
-printf("%c",va_arg(args, int));
+printf("%s%c", se, va_arg(args, int));
 break;
 case 'i':
-printf("%d",va_arg(args, int));
+printf("%s%d", se, va_arg(args, int));
 break;
 case 'f':
-printf("%.6f",va_arg(args, double));
+printf("%s%.6f", se, va_arg(args, double));
 break;
 case 's':
 pe = va_arg(args, char *);
 if (pe == NULL)
 pe = "(nil)";
-printf("%s", pe);
+printf("%s%s", se, pe);
 break;
 default:
 i++;
 continue;
 }
 i++;
-if (format[i])
-printf(", ");
+se = ", ";
 }
 }
- printf("\n");
- va_end(args);
+printf("\n");
+va_end(args);
 }
