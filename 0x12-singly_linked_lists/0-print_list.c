05339eefb6fb802dafc035606808593937e06443
AbdiAbader <abdiprocode@gmail.com>
2022-07-14 05:42:16 -0700

    create files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *size_t - structure node 
+ *@h: head
+ *Return: void
+ */
+size_t print_list(const list_t *h)
+{
+  int c =0; 
+
+
+while (h != NULL)
+{
+if(h->str == NULL)
+printf("[%d] %s\n", 0, h->str);
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+c++;
+}
+return (c);
+}
