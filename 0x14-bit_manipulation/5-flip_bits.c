df8028d090964b2e46a0ca7fc2287fb2bb195f29
AbdiAbader <abdiprocode@gmail.com>
2022-07-21 20:06:35 +0300

    flips

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*flip_bits - set bit at given index
+*@n: number
+*@m: index
+*Return: 1 if sucess -1 otherwise
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+{
+unsigned long int flips, i;
+while (filps)
+{
+if (filps & 1)
+i++;
+filps = filps >> 1;
+}
+return (i);
+}
