d18a13395655bfc304df4e63d19640623615d8bf
AbdiAbader <abdiprocode@gmail.com>
2022-07-21 20:11:32 +0300

    flips

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,14 +5,15 @@
 *@m: index
 *Return: 1 if sucess -1 otherwise
 */
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned long int flips, i;
-while (filps)
+unsigned long int flips, i = 0;
+flips = n ^ m;
+while (flips)
 {
-if (filps & 1)
+if (flips & 1)
 i++;
-filps = filps >> 1;
+flips = flips >> 1;
 }
 return (i);
 }
