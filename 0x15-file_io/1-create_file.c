b7fb5a6fa8beb0d9063c3a749c8d0a9b0d60936a
AbdiAbader <abdiprocode@gmail.com>
2022-07-25 18:38:21 +0300

    header file updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,16 +8,17 @@
 int create_file(const char *filename, char *text_content)
 {
 int i = 0, o, w;
-o = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (o == -1)
-return (0);
+return (-1);
+if (text_content != NULL)
+{
 while (text_content[i])
 i++;
-if (!text_content)
-text_content = "";
+}
 w = write(o, text_content, i);
 if (w == -1)
-return (0);
+return (-1);
 close(o);
 return (1);
 }
