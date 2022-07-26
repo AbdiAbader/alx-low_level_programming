8e78641e2eae773c7d96cff8bcd06c0a3a59fa5d
AbdiAbader <abdiprocode@gmail.com>
2022-07-26 16:43:49 +0300

    final update on read text for commad arg

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,33 +7,22 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *str;
-ssize_t f, r, w;
+ssize_t fdr, fdw, fdo;
+char *holder;
 if (filename == NULL)
 return (0);
-str = malloc(sizeof(char) * letters);
-if (str == NULL)
+holder = malloc(sizeof(char) * letters);
+if (holder == NULL)
 return (0);
-else
+fdo = open(filename, O_RDONLY);
+fdr = read(fdo, holder, letters);
+fdw = write(STDOUT_FILENO, holder, fdr);
+if (fdo == -1 || fdr == -1 || fdw == -1 || fdw != fdr)
 {
-f = open(filename, O_RDONLY);
-if (f == -1)
-return (0);
-r = read(f, str, letters);
-if (r == -1)
-{
-free(str);
+free(holder);
 return (0);
 }
-w = write(STDOUT_FILENO, str, r);
-if (w == -1)
-{
-if (w != r)
-free(str);
-return (0);
-}
-free(str);
-close(f);
-return (w);
-}
+free(holder);
+close(fdo);
+return (fdw);
 }
