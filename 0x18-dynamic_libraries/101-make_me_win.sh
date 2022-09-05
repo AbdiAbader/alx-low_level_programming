43811d02397b1ed606fa155afc5c3a198946e177
AbdiAbader <abdiprocode@gmail.com>
2022-09-05 18:35:46 +0300

    fix error on wget

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q-O r/librand.so https://github.com/AbdiAbader/alx-low_level_programming/raw/master/0x18-dynamic_libraries/librand.so
-export LD_PRELOAD=r/librand.so
+wget -q-O /run/librand.so https://github.com/AbdiAbader/alx-low_level_programming/raw/master/0x18-dynamic_libraries/librand.so
+export LD_PRELOAD=/run/librand.so
