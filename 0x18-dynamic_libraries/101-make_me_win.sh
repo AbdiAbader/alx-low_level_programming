42289ec290aad92bb77c75f692d05794ff7bf5f5
AbdiAbader <abdiprocode@gmail.com>
2022-09-05 16:31:46 +0300

    script file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl /tmp https://github.com/AbdiAbader/alx-low_level_programming/tree/master/0x18-dynamic_libraries/librand.o
+curl --capath /tmp https://github.com/AbdiAbader/alx-low_level_programming/tree/master/0x18-dynamic_libraries/librand.o
 export LD_PRELOAD="/tmp/librand.o"
