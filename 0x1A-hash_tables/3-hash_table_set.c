2bd3060a2884285cc2882dfffc9d99e8ade09685
AbdiAbader <abdiprocode@gmail.com>
2022-09-22 19:48:48 +0300

    fix error on strdup and strcpy

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,26 +8,32 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long int x;
-hash_node_t *new , *old;
-if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-return (0);
-x = key_index((const unsigned char *)key, ht->size);
-old = ht->array[x]; 
-while (old != NULL)
-{
-if (strcmp(old->key, key) == 0)
-{
-free(old->value);
-strcpy(old->value, value);
-return (1);
-}
-old = old->next;
-}
-new = malloc(sizeof(hash_node_t));
+hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
-		
+		free(value_copy);
 		return (0);
 	}
 	new->key = strdup(key);
@@ -36,9 +42,9 @@ new = malloc(sizeof(hash_node_t));
 		free(new);
 		return (0);
 	}
-	strcpy(old->value, value);
-	new->next = ht->array[x];
-	ht->array[x] = new;
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
