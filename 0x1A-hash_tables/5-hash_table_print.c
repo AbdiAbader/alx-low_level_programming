3a6f3aaa336cb15899f0972a7de8ecf8c5fb6ba4
AbdiAbader <abdiprocode@gmail.com>
2022-09-23 16:15:18 +0300

    update

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,24 +7,24 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-int a = 0;
-hash_node_t *temp;
-unsigned long int i = 0;
-if (ht != NULL)
-{
-printf("{");
-for (i = 0 ; i < ht->size; i++)
-{
-temp = ht->array[i];
-while (temp != NULL)
-{
-if (a != 0)
-printf(", ");
-printf("'%s': '%s'", temp->key, temp->value);
-temp = temp->next;
-a = 87;
-}
-}
-printf("}\n");
-}
+unsigned long int i;
+	hash_node_t *tmp;
+	char flag = 0; /* 0 while no data has been printed yet */
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			flag = 1;
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
 }
